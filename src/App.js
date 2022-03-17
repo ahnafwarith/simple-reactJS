50aee85e1889a5c6a59e24cbdd8462c1879c0e7b
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-18 05:54:23 +0600

    added new stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <ChangeState></ChangeState>
     </div>
   );
 }
 
-function LoadCountries() {
+function ChangeState() {
+  const [count, setCount] = useState(55)
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount)
+  }
+  const decreaseCount = () => setCount(count - 1);
   return (
     <div>
-      <h1>Loading from Rest Countries API</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   );
 }
